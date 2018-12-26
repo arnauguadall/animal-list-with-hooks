f1b5823aa4281d0fa11bd3cd1897e01213d383c4
Arnau Guadall <guadall@strands.com>
2018-12-26 21:19:52 +0100

    shorten comments to improve readability

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -3,17 +3,18 @@ import axios from 'axios';
 import './List.css';
 
 /* 
-	The class declaration 'class List extends Component' will be replaced for 'export default function List()'
-	in order every 'component' will be a function and no longer we will declare a React Class.
+	The class declaration 'class List extends Component' will be replaced for
+	'export default function List()' in order every 'component' will be a
+	function and no longer we will declare a React Class.
 */
 export default function List() {
 	/* 
 		State variables will be defined separetly with a method to execute them and a initial value.
 
-		dogList:     State variable
-		setdogList:  Method to set the value to 'dogList' variable
-		useState: 		Work the same was as default props giving the 'dogList'
-				   		an initial value in this case an empty array.
+		dogList:    State variable
+		setdogList: Method to set the value to 'dogList' variable
+		useState: 	Work the same was as default props giving the 'dogList'
+				   	an initial value in this case an empty array.
 	*/
 	const [dogList, setdogList] = useState([]);
 
@@ -21,10 +22,12 @@ export default function List() {
 	useCustomTitleChange("Dog list");
 
 	/* 
-		The old lifecycle (componentDidMount, componentDidUpdate, componentWillUnmount, ...) has been simplified
-		by using 'useEffect' and this method will be able to subscribe and unsubscribe from an endpoint call for example.
+		The old lifecycle (componentDidMount, componentDidUpdate, componentWillUnmount, ...)
+		has been simplified by using 'useEffect' and this method will be able to subscribe and
+		unsubscribe from an endpoint call for example.
 
-		Also, you can have many 'useEffect' methods to take care of several functionalities you want to use.
+		Also, you can have many 'useEffect' methods to take care of several
+		functionalities you want to use.
 	*/
 	useEffect(() => {
 		axios.get(`https://dog.ceo/api/breeds/list/all`)
@@ -35,8 +38,9 @@ export default function List() {
 	})
 
 	/* 
-		Custom HOOK to change the title when the webpage refreshes. In this case we could use this with an input change,
-		button event or if we receive some data the 'useEffect' will know when it need to rerender and execute the effect.
+		Custom HOOK to change the title when the webpage refreshes. In this case we could use it
+		this with an input change, button event or if we receive some data the 'useEffect'
+		will know when it need to rerender and execute the effect.
 	*/
 	function useCustomTitleChange(title) {
 		useEffect(() => {
