2645de40addd6abfc34a0f5589345edaf970bc4c
Arnau Guadall <guadall@strands.com>
2018-12-26 21:20:51 +0100

    formatting files

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import List from './components/List/List';
 
 
 export default function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<code>Animal List</code>
-				<List />
-			</header>
-		</div>
-	);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <code>Animal List</code>
+                <List />
+            </header>
+        </div>
+    );
 }
--- src/components/list/list.js
+++ src/components/list/list.js
@@ -16,10 +16,10 @@ export default function List() {
 		useState: 	Work the same was as default props giving the 'dogList'
 				   	an initial value in this case an empty array.
 	*/
-	const [dogList, setdogList] = useState([]);
+    const [dogList, setdogList] = useState([]);
 
-	// Giving the value to our custom HOOK.
-	useCustomTitleChange("Dog list");
+    // Giving the value to our custom HOOK.
+    useCustomTitleChange("Dog list");
 
 	/* 
 		The old lifecycle (componentDidMount, componentDidUpdate, componentWillUnmount, ...)
@@ -29,31 +29,31 @@ export default function List() {
 		Also, you can have many 'useEffect' methods to take care of several
 		functionalities you want to use.
 	*/
-	useEffect(() => {
-		axios.get(`https://dog.ceo/api/breeds/list/all`)
-			.then(response => {
-				// Setting 'dogList' by executing 'setdogList' when the data has been received.
-				setdogList(response.data.message);
-			})
-	})
+    useEffect(() => {
+        axios.get(`https://dog.ceo/api/breeds/list/all`)
+            .then(response => {
+                // Setting 'dogList' by executing 'setdogList' when the data has been received.
+                setdogList(response.data.message);
+            })
+    })
 
 	/* 
 		Custom HOOK to change the title when the webpage refreshes. In this case we could use it
 		this with an input change, button event or if we receive some data the 'useEffect'
 		will know when it need to rerender and execute the effect.
 	*/
-	function useCustomTitleChange(title) {
-		useEffect(() => {
-			document.title = title;
-		})
-	}
+    function useCustomTitleChange(title) {
+        useEffect(() => {
+            document.title = title;
+        })
+    }
 
-	return (
-		<ul>
-			{Object.keys(dogList).map(dog =>
-				<li key={dog}>
-					{dog}
-				</li>)}
-		</ul>
-	);
+    return (
+        <ul>
+            {Object.keys(dogList).map(dog =>
+                <li key={dog}>
+                    {dog}
+                </li>)}
+        </ul>
+    );
 }
